06be311933d7b5ff4653981a72e26a5e435ef8d7
mishaells <163031924+mishaells@users.noreply.github.com>
2024-11-10 18:44:16 +0300

    added boo

--- meshGitTry/src/meshGitTry/simpleClass.java
+++ meshGitTry/src/meshGitTry/simpleClass.java
@@ -5,6 +5,7 @@ public class simpleClass {
 	public static void main(String [] args) {
 		System.out.println("mishael");
       System.out.println("hi everyone im here");
+      System.out.println("boo");
 	}
 
 }
